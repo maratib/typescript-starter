4c8b8d75755905ade5b7fb3af11424e9a6445478
Maratib Ali Khan <maratib@gmail.com>
2024-02-20 01:38:53 +0500

    tests taken out from src folder

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { addition } from "..";
+import { addition } from "@/index";
 
 describe("Calculator Tests", () => {
   it("should return 5 when 2 is added to 3", () => {
@@ -11,5 +11,6 @@ describe("Calculator Tests", () => {
 describe("Calc Tests", () => {
   it("Hello Test", async () => {
     console.log("Hello test");
+    console.log(addition(10, 30));
   });
 });
