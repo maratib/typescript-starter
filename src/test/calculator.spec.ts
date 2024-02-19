e232c7674d732cc73b2fe5f8ad65fdfbd7869d43
Maratib Ali Khan <maratib@gmail.com>
2024-02-19 23:33:52 +0500

    final

--- src/test/calculator.spec.ts
+++ src/test/calculator.spec.ts
@@ -2,8 +2,14 @@ import { assert } from "chai";
 import { addition } from "..";
 
 describe("Calculator Tests", () => {
-      it("should return 5 when 2 is added to 3", () => {
-      const result = addition(2, 3);
-      assert.equal(result, 5);
-   });
+  it("should return 5 when 2 is added to 3", () => {
+    const result = addition(2, 3);
+    assert.equal(result, 5);
+  });
+});
+
+describe("Calc Tests", () => {
+  it("Hello Test", async () => {
+    console.log("Hello test");
+  });
 });
